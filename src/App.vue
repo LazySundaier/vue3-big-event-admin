ac3ed470748632ebb3f726b6809925c1d0ab8c0b
2795402901@qq.com <2795402901@qq.com>
2024-05-10 23:18:43 +0800

    pinia统一管理

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
-<script setup></script>
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+import { useUserStore } from '@/stores'
+const route = useRoute()
+const router = useRouter()
+const go = () => {
+  console.log(router, route)
+}
+const userStore = useUserStore()
+</script>
 
 <template>
   <div>app</div>
+  <el-button @click="$router.push('/home')">1</el-button>
+  <el-button @click="go">2</el-button>
+  <hr />
+  <p>{{ userStore.token }}</p>
+  <el-button @click="userStore.setToken('1111')">登录</el-button>
+  <el-button @click="userStore.removeToken">登出</el-button>
 </template>
 
 <style scoped></style>
