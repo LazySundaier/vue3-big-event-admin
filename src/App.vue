faef88319a7255f16f71ff461b56df23fef2643a
2795402901@qq.com <2795402901@qq.com>
2024-05-11 19:52:25 +0800

    实现登录

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,7 @@
-<script setup>
-import { useRoute, useRouter } from 'vue-router'
-import { useUserStore } from '@/stores'
-const route = useRoute()
-const router = useRouter()
-const go = () => {
-  console.log(router, route)
-}
-const userStore = useUserStore()
-</script>
+<script setup></script>
 
 <template>
   <router-view></router-view>
-  <hr />
-  <div>app</div>
-  <el-button @click="$router.push('/home')">1</el-button>
-  <el-button @click="go">2</el-button>
-  <hr />
-  <p>{{ userStore.token }}</p>
-  <el-button @click="userStore.setToken('1111')">登录</el-button>
-  <el-button @click="userStore.removeToken">登出</el-button>
 </template>
 
 <style scoped></style>
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,5 @@
 import { useUserStore } from '@/stores'
 import axios from 'axios'
-import { ElMessage } from 'element-plus'
 import router from '@/router'
 
 const baseURL = 'http://big-event-vue-api-t.itheima.net'
